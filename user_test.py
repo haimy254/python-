8cb207d9919c0af167248ee7402eaa75729a2241
haimy254 <haimanaare@gmail.com>
2022-04-25 10:38:20 +0300

    test unit

--- user_test.py
+++ user_test.py
@@ -2,59 +2,19 @@ import unittest
 # import pyperclip
 from users import User
 
-def setUp(self):
-    self.new_user=User("waimina","1234567")
+class test_users(unittest.TestCase):
 
-def __init__(self):
-         self.assertEqual(self.new_user.username,"waimana")
-         self.assertEqual(self.new_user.password,"1234567")
-      
-def test_init(self):
-    User.user_list=[] 
+    def setUp(self):
+        self.new_user=User("waimina","1234567")
+
+    def test_init(self):
+            self.assertEqual(self.new_user.username,"waimina")
+            self.assertEqual(self.new_user.password,"1234567")
+        
+   
+    
     
 if __name__=='__main__':
-    unittest.main()
+        unittest.main()
     
-    # print("good")
-# def setUp(self):
-#     self.new_user=User("haimana", "987456")
-    
-# def test_inti(self):
-#     self.assertEqual(self.new_user.username,"haimana")
-#     self.assertEqual(self.new_user.password,"987456")
-    
-    
-#          print("Do you have an account")
-      
-# input =user(YES ="login"),( NO = "create new account")
-         
-# if not YES:
-#     print("login in your account")
-    
-# else:
-#     print("create new account")
-# def create_new_account(self,username, password):
-#     self.username= username
-#     self.password= password
-#     return
-# def test_self_user(self):
-#     self.new_user.save_user()
-#     self.assertEqual(len(User.user_list),1)
-    
-
-    
-# user_have_an_account=input("do you have an existing account? ")
-
-
-# if(user_have_an_account):
-#   def test_display_all_user(self):
-#       self.assertEqual(Account.displpay_users())
-#     #if user has an account 
-#     #1) Receive credentials (username and password)
-#     #2) Check credentials
-#     #3) view your credentials
-#     #4) you can delete 
-# else:
-#     #if uuser doesn't have an account 
-#     #create an account 
-#     # save the account 
+  
