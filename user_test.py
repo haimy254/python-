cba1b2f709fac124a8cc680d7aecc7f2109fede3
haimy254 <haimanaare@gmail.com>
2022-04-25 13:39:25 +0300

    multiple user

--- user_test.py
+++ user_test.py
@@ -1,4 +1,3 @@
-
 import unittest
 # import pyperclip
 from users import User
@@ -12,12 +11,22 @@ class test_users(unittest.TestCase):
         self.assertEqual(self.new_user.username,"waimina")
         self.assertEqual(self.new_user.password,"1234567")
         
-    
-        
     def test_save_user(self):
         self.new_user.save_user()
         self.assertEqual(len(User.user_list),1)
     
+    def tearDown(self):
+        User.user_list=[]
+        
+    def test_save_multiple_user(self):
+        self.new_user.save_user()
+        test_user = User("Test", "user",)
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
+     
+    
+           
+        
     
     
 if __name__=='__main__':
